9067ef2f93b0043c553259949b937ee176a4b53a
Prashanth Boovaragavan <prashanth689@gmail.com>
2025-05-30 18:23:26 -0700

    Forcing deployment

--- src/server.ts
+++ src/server.ts
@@ -64,6 +64,7 @@ app.use(helmet()); // Security headers
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
+
 // 🔹 CORS Configuration
 app.use(
   cors({
